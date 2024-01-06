140592b9a7919af533cd71b0b2102e64d5a0d94c
filizhelvaci <filizhelvaci82@gmail.com>
2024-01-06 11:58:24 +0300

    Main.java eklendi

---
+++ Main.java
@@ -0,0 +1,15 @@
+package AdventurGm;
+
+public class Main {
+
+	public static void main(String[] args) {
+			
+		System.out.println("selam oyun başlamak üzere...."); 
+		Game game=new Game();
+		game.login();
+		
+		
+
+	}
+
+}
