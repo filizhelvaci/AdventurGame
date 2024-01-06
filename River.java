b542c5e6864bc2497f09c7565c00318bdf3486ff
filizhelvaci <filizhelvaci82@gmail.com>
2024-01-06 12:01:28 +0300

    River.java eklendi

---
+++ River.java
@@ -0,0 +1,12 @@
+package AdventurGm;
+
+public class River extends BattleLoc{
+
+	River(Player player) {
+		super(player, "River", new Bear(),"Water" );
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
